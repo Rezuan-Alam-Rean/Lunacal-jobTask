17f25c72a2f3592187f1117520d76ff8e3c1c8c2
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-09-06 21:42:14 +0600

    divided wight for large devices

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,18 @@
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className="w-full md:flex">
+            <div className="w-1/2 bg-slate-700" >
+              
+            </div>
+            <div className="w-1/2 bg-red-300">
+                <div>
+                    <p>widget1</p>
+                </div>
+                <div>
+                    <p>widget2</p>
+                </div>
+            </div>
         </div>
     );
 };
