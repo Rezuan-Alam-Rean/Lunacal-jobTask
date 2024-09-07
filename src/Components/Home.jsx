d65438749f71176e07ae4c74039170e873627c34
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-09-07 22:55:55 +0600

    divided wight for large devices

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,16 @@ const Home = () => {
             </div>
             <div className="w-1/2 bg-red-300">
                 <div>
-                    <p>widget1</p>
+                    <div>
+                        <p>About Me</p>
+                        <p>Experiences</p>
+                        <p>Recommended</p>
+                    </div>
+                    <div>
+                        <p>Hello! I’m Dave, your sales rep here from Salesforce. I’ve been working at this awesome company for 3 years now.
+
+I was born and raised in Albany, NY& have been living in Santa Carla for the past 10 years my wife Tiffany and my 4 year old twin daughters- Emma and Ella. Both of them are just starting school, so my calender is usually blocked between 9-10 AM. This is a...</p>
+                    </div>
                 </div>
                 <div>
                     <p>widget2</p>
